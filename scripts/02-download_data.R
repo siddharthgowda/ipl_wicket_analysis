4e538cdb71b271f685675f2be7ac38d862f211d8
Siddharth Gowda <siddharth@skube.xyz>
2024-11-28 14:47:08 -0500

    EDA for boudaries in ipl cricket

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,26 +1,33 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from cricketdata package
+# Author: Siddharth Gowda
+# Date: 28 November 2024
+# Contact: siddharth.gowda@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Install cricketdata R package and general understanding of
+# the sport of cricket
+# Any other information needed? None
 
 
 #### Workspace setup ####
-library(opendatatoronto)
+#install.packages("cricketdata", dependencies = TRUE)
+library(cricketdata)
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
+ipl_bbb <- fetch_cricsheet("bbb", "male", "ipl")
+ipl_bbb %>% glimpse()
+ipl_bbb %>% head()
+player_meta <- player_meta
+player_meta %>% glimpse()
+player_meta %>% head()
 
 
 #### Save data ####
 # [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(ipl_bbb, "data/01-raw_data/ipl_men.csv")
+write_csv(player_meta, "data/01-raw_data/player_meta.csv")
 
          
--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,44 +1,50 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the raw ipl and player data to only include ipl players and boundary deliveries
+# Author: Siddharth Gowda
+# Date: 28 November 2024
+# Contact: siddharth.gowda@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: tidyverse, lubridate, arrow packages must be installed
+# Any other information needed? None
 
 #### Workspace setup ####
 library(tidyverse)
+library(lubridate)
+library(arrow)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+ipl_men_raw <- read_csv("data/01-raw_data/ipl_men.csv")
+player_meta_raw <- read_csv("data/01-raw_data/player_meta.csv")
+ipl_men_raw %>% glimpse()
+player_meta_raw %>% glimpse()
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+ipl_men_clean <- ipl_men_raw %>%  mutate(year = year(ymd(start_date))) %>%
+  select(!season) %>% select(!start_date) %>% 
+  select(match_id, year, venue, innings, over, ball, batting_team, bowling_team, 
+         striker, bowler, runs_off_bat, wickets_lost_yet, target)
+
+ipl_men_clean_no_two_innings <- ipl_men_clean %>% filter(innings > 2)
+
+ipl_men_clean <- ipl_men_clean %>% 
+  filter(!(match_id %in% ipl_men_clean_no_two_innings$match_id)) %>%
+  # only boundaries
+  mutate(is_boundary = (runs_off_bat >= 4))
+
+player_meta_clean <- player_meta_raw %>% select(unique_name, batting_style, bowling_style, playing_role)
+
+cleaned_data <- left_join(ipl_men_clean, player_meta_clean, by=c('striker' = 'unique_name')) %>% 
+  select(!bowling_style) %>% rename(batter_playing_role = playing_role)
+
+cleaned_data <- left_join(cleaned_data, (player_meta_clean %>% select(!batting_style)), 
+                          by=c('bowler' = 'unique_name')) %>% 
+  rename(bowler_playing_role = playing_role)
+
+cleaned_data <- cleaned_data %>%
+  filter(!is.na(bowler_playing_role)) %>%
+  filter(!is.na(batter_playing_role)) %>%
+  filter(!is.na(batting_style)) %>%
+  filter(!is.na(bowling_style)) %>%
+  filter(!grepl(",", bowling_style))
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_parquet(cleaned_data, "data/02-analysis_data/cleaned_data.parquet")
